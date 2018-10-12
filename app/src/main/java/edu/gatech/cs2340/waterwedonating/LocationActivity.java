15e6b942aa60a512dda7d24f4883d8098f619cba
Eva <ychen840@gatech.edu>
2018-10-11 20:28:55 -0400

    Parse the location data and edited the main list view.

--- app/src/main/java/edu/gatech/cs2340/waterwedonating/LocationActivity.java
+++ app/src/main/java/edu/gatech/cs2340/waterwedonating/LocationActivity.java
@@ -2,14 +2,18 @@ package edu.gatech.cs2340.waterwedonating;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Parcelable;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.io.InputStream;
+import java.util.List;
 
 public class LocationActivity extends Activity {
 
     private Button backButton;
-    private TextView testScreen;
+    private ListView locationList;
+    private ItemArrayAdapter itemArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +21,20 @@ public class LocationActivity extends Activity {
         setContentView(R.layout.activity_location);
 
         backButton = findViewById(R.id.backButton);
-        testScreen = findViewById(R.id.testScreen);
+        locationList = findViewById(R.id.locationList);
+        itemArrayAdapter = new ItemArrayAdapter(getApplicationContext(), R.layout.item_layout);
 
+        Parcelable state = locationList.onSaveInstanceState();
+        locationList.setAdapter(itemArrayAdapter);
+        locationList.onRestoreInstanceState(state);
+
+        InputStream inputStream = getResources().openRawResource(R.raw.locationdata);
+        LocationReader locationReader = new LocationReader(inputStream);
+        List<String[]> dataList = locationReader.read();
+
+        for(String[] data:dataList ) {
+            itemArrayAdapter.add(data);
+        }
     }
 
 }
