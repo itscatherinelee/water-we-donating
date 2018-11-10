fed90d278091fdf4e5390ef9b6b7cbf3ef26f257
itscatherinelee <itscatherinehlee@gmail.com>
2018-11-10 17:14:59 -0500

    junit testing

---
+++ app/src/test/java/edu/gatech/cs2340/waterwedonating/CatherineJUnitTest.java
@@ -0,0 +1,19 @@
+package edu.gatech.cs2340.waterwedonating;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class CatherineJUnitTest {
+    private ArrayList<donationData> list;
+    @Before
+    public void setUp() {
+        list = new ArrayList<>();
+    }
+    @Test
+    public void notNull() {
+        assertEquals(false);
+    }
+}
