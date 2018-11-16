821f8fa15fe07302f763a11c87f8084d6358cc60
lost_In_The_Code4ever <Naimkabir14@users.noreply.github.com>
2018-11-16 13:43:49 -0500

    Updated junit test

--- app/src/test/java/edu/gatech/cs2340/waterwedonating/naimJunitTest.java
+++ app/src/test/java/edu/gatech/cs2340/waterwedonating/naimJunitTest.java
@@ -2,29 +2,37 @@ package edu.gatech.cs2340.waterwedonating;
 import org.junit.Before ;
 import org.junit.Test ;
 
-import java.util.ArrayList;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 
 /**
- * Naim's javadoc for testing RegistrationActivity.register() method
+ * Naim's javadoc for testing RegistrationActivity.LoginUser() method
  */
 public class naimJunitTest {
-    private RegistrationActivity rA;
-    ArrayList<String> lists;
+    public LoginActivity rA;
     @Before
     public void setUp() {
-        rA = new RegistrationActivity();
+        rA = new LoginActivity();
     }
     @Test
     public void testTrue() {
-        assertTrue(rA.register("sdjfsjfhsd","dsjchjdshcjsd"));
+        assertTrue(rA.LoginUser("ewhdhwdkjwd","erfjekfmewf"));
     }
     @Test
     public void testFalse() {
-        assertFalse("The parameters are null", rA.register("",""));
+        assertFalse(rA.LoginUser("",""));
+    }
+    @Test
+    public void checkEqual() {
+        assertEquals(false, rA.LoginUser("",""));
+        assertEquals(true, rA.LoginUser("sdffwef","weffwefwef"));
+    }
+    @Test
+    public void notNull() {
+        assertNotNull(rA);
     }
 
  }
