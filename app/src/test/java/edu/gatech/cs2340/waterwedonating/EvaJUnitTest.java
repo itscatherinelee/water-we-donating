30b03c4f5345d998f49e8f86c43e11978fca253e
evachenyr <42756393+evachenyr@users.noreply.github.com>
2018-11-16 10:53:44 -0500

    Create Junit test

---
+++ app/src/test/java/edu/gatech/cs2340/waterwedonating/EvaJUnitTest.java
@@ -0,0 +1,50 @@
+package edu.gatech.cs2340.waterwedonating;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class EvaJUnitTest {
+
+    private InputStream testingInput = null;
+    private InputStream validInput = new ByteArrayInputStream("A,B,C".getBytes());
+
+    @Test
+    public void locationReaderTest() {
+
+        try {
+
+            LocationReader locationReader = new LocationReader(testingInput);
+
+        } catch (IllegalArgumentException e) {
+
+            assertEquals(e.getMessage(), "inputStream cannot be null.");
+        }
+
+
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void nullInputStream() {
+
+        LocationReader locationReader = new LocationReader(null);
+
+       ArrayList arrayList = (ArrayList) locationReader.read();
+
+       assertTrue(arrayList.size() > 0);
+
+    }
+
+    @Test
+    public void validInputStream() {
+
+        LocationReader locationReader = new LocationReader(validInput);
+
+        assertNotNull(locationReader.inputStream);
+        assertSame(locationReader.inputStream, validInput);
+    }
+}
